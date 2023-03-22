bb705a7195226e9318e0d5f12583e228183eb2fc
PMclaug1 <pmclaug1@gmail.com>
2023-03-22 15:13:53 -0500

    daily algo

--- singly_linked_list.js
+++ singly_linked_list.js
@@ -75,14 +75,37 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
+    //return value by list position
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        var counter = 0;
+        var current = this.head;
+        while(counter !== index){
+            current = current.next;
+            counter++;
+        }
+        return current
+    }
+    //Use get method to find node. Update value and return true. (False if not present)
+    set(index, val){
+        var foundNode = this.get(index);
+        if(foundNode){
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
 }
 
 var list = new SinglyLinkedList();
 list.push("Hello")
 list.push("Goodbye")
 list.push("!")
+list.push("Next")
 list.pop()
 list.shift()
 list.unshift("Test")
+console.log(list.get(2))
+list.set(2, "Update")
 
 console.log(list)
