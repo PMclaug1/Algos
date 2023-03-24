06f559702a286ae49821e6ee6588a8c1741eb29b
PMclaug1 <pmclaug1@gmail.com>
2023-03-24 17:17:26 -0500

    DLL

---
+++ doubly_linked_list.js
@@ -0,0 +1,34 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.previous = null;
+    }
+}
+
+class DoublyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            this.tail.next = newNode;
+            newNode.previous = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+list = new DoublyLinkedList()
+list.push(5)
+
+console.log(list)
