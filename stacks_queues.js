295d3be3ca0fbd4f912793a997f6ca05e8931eaf
PMclaug1 <pmclaug1@gmail.com>
2023-04-12 22:10:28 -0500

    stacks_queues

--- stacks_queues.js
+++ stacks_queues.js
@@ -3,9 +3,55 @@
 //can also use shift/unshift, but this is less efficient
 //because you are adding/removing from the beginning(must reindex whole array each time)
 //for large sets of data arrays are inefficent. Linked list is preferable
-var stack = []
-stack.push("create new file")
-stack.push("resized file")
-stack.push("cloned out blur")
+// var stack = []
+// stack.push("create new file")
+// stack.push("resized file")
+// stack.push("cloned out blur")
+// stack.pop()
+// console.log(stack)
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    push(val) {
+        var newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        }
+        else {
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size
+    }
+    pop(){
+        if(!this.first) return null;
+        var temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
+
+var stack = new Stack()
+stack.push(23)
+stack.push(37)
+stack.push(45)
 stack.pop()
+
 console.log(stack)
